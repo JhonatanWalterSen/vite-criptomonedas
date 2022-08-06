aa290422ab810370261c995242df6dfb329532d3
Jhonatan Ariza <arizaj@LIM915010.local>
2022-08-06 01:47:56 -0500

    Inicio de criptomonedas en react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Titillium Web', sans-serif;
+
+`
+
+function App() {
+  return (
+    <h1>Hi</h1>
+  )
+}
+
+export default App

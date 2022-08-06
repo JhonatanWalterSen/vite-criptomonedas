0b72ec92c0c74e6c4eff2529a8a692719619d6d4
Jhonatan Ariza <arizaj@LIM915010.local>
2022-08-06 02:22:55 -0500

    usando styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import imagenCripto from './img/imagen-criptos.png'
+
+const Contenedor= styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width:90%;
+`
+
+const imagen = styled.img`
+  max-width:400px;
+  width:80%;
+  margin: 100px 0 auto 0;
+  display:block;
+`
 
 const Heading = styled.h1`
   font-family: 'Titillium Web', sans-serif;
-
+  color: #fff;
 `
 
 function App() {
   return (
-    <h1>Hi</h1>
+    <Contenedor>
+      <Heading>Desde App Jsx</Heading>
+    </Contenedor>
   )
 }
 

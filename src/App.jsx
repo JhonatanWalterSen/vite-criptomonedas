9373596a0aa8e2efa735c72410318ea5ac8c4b75
Jhonatan Ariza <arizaj@LIM915010.local>
2022-08-06 03:26:55 -0500

    usando mi propio hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 import imagenCripto from './img/imagen-criptos.png'
+import Formulario from './Components/Formulario'
+
+
 
 const Contenedor= styled.div`
   max-width: 900px;
   margin: 0 auto;
   width:90%;
+  @media (min-width: 990px) {
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap:2rem;
+  }
 `
 
-const imagen = styled.img`
+const Imagen = styled.img`
   max-width:400px;
   width:80%;
   margin: 100px 0 auto 0;
@@ -18,12 +26,33 @@ const imagen = styled.img`
 const Heading = styled.h1`
   font-family: 'Titillium Web', sans-serif;
   color: #fff;
+  text-align:center;
+  font-weight:700;
+  margin-top:80px;
+  margin-bottom:50px;
+  font-size:34px;
+
+  ::after{
+    content:'';
+    width:100px;
+    height: 7px;
+    background-color: #66a2fe;
+    display:block;
+    margin: 10px auto 0 auto;
+  }
 `
 
 function App() {
   return (
     <Contenedor>
-      <Heading>Desde App Jsx</Heading>
+      <Imagen
+        src={imagenCripto}
+        alt="Imagen Criptomonedas">
+      </Imagen>
+      <div>
+        <Heading>Cotiza Monedas al Instantex</Heading>
+        <Formulario />
+      </div>
     </Contenedor>
   )
 }
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+    const SelectMonedas = () =>(
+        <>
+            <label>Selecccionar monedas</label>
+        </>
+    )
+    return [SelectMonedas]
+}
+
+export default useSelectMonedas
